de8c7c78d8ec9495c9f2f7fe8141c87ac105c208
Mikael Fructuoso <mika.fr99@hotmail.com>
2023-09-21 01:34:30 +0200

    Portfolio update 2023
    - Anirya + Jurassic snap + Alpha 6 and Mappa mundi text + page

--- portfolio.js
+++ portfolio.js
@@ -12,21 +12,25 @@ app.get('/temp-page.html', (req, res) => {
     res.sendFile('/pages/temp-page.html', { root: __dirname });
 });
 
-/*app.get('/MappaMundi.html', (req, res) => {
+app.get('/MappaMundi.html', (req, res) => {
     res.sendFile('/pages/MappaMundi.html', { root: __dirname });
-});*/
+});
 
-/*app.get('/Alpha_6.html', (req, res) => {
+app.get('/Alpha_6.html', (req, res) => {
     res.sendFile('/pages/Alpha_6.html', { root: __dirname });
-});*/
+});
+
+app.get('/new_project_template_Copie.html', (req, res) => {
+    res.sendFile('/pages/new_project_template_Copie.html', { root: __dirname });
+});
 
 app.get('/The_Morrigan.html', (req, res) => {
     res.sendFile('/pages/The_Morrigan.html', { root: __dirname });
 });
 
-/*app.get('/Anirya_and_the_upside-down_world.html', (req, res) => {
+app.get('/Anirya_and_the_upside-down_world.html', (req, res) => {
     res.sendFile('/pages/Anirya_and_the_upside-down_world.html', { root: __dirname });
-});*/
+});
 
 app.get('/Jurassic_Snap.html', (req, res) => {
     res.sendFile('/pages/Jurassic_Snap.html', { root: __dirname });
